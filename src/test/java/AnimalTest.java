98d3cc6cdb9760a6b0f715e1f6faae2c9efaebb9
Annette Odingo <odingoannette@gmail.com>
2019-07-01 06:39:23 +0300

    Check Animal constructor instantiates correctly

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,5 +1,19 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AnimalTest {
+    @Test
+    public void animalInstantiatesCorrectly() {
+        Animal newAnimal = setupNewAnimal();
+        assertEquals(true, newAnimal instanceof Animal);
+    }
+
+
+
+    public Animal setupNewAnimal(){
+        return new Animal("lion");
+    }
+
 
 }
