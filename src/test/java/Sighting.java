43e20ad9529f81efc02b943f4bb3c18726a30611
Annette Odingo <odingoannette@gmail.com>
2019-07-03 20:09:16 +0300

    Add a constructor and create SightingTest.java

--- src/test/java/Sighting.java
+++ src/test/java/Sighting.java
@@ -1,2 +1,12 @@
 public class Sighting {
+    private int id;
+    private String Location;
+    private String Ranger;
+    private  int animalId;
+
+    public Sighting(String location, String ranger, int animalId) {
+        Location = location;
+        Ranger = ranger;
+        this.animalId = animalId;
+    }
 }
