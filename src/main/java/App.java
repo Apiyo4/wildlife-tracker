9416be07e5bac11393bc6229ac5b0cfc597b0b14
Annette Odingo <odingoannette@gmail.com>
2019-07-05 00:24:50 +0300

    Add dashboard routes and make sure it functions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -29,7 +30,60 @@ public class App {
             return new ModelAndView(model, "form.hbs");
         },new HandlebarsTemplateEngine());
 
+        post("/dashboards", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            String location = request.queryParams("location");
+            String rangerName = request.queryParams("rangerName");
+            String badgeNumber = request.queryParams("badgeNumber");
+            String animalName = request.queryParams("animalName");
+            String animalType = request.queryParams("animalType");
+            String animalHealth = request.queryParams("animalHealth");
+            String animalAge = request.queryParams("animalAge");
 
+            if (!(rangerName.trim().isEmpty()|| badgeNumber.trim().isEmpty())) {
+                Ranger ranger = new Ranger(rangerName, Integer.parseInt(badgeNumber));
+                ranger.save();
+            }else if (animalType.equals("Endangered") && (!(animalName.trim().isEmpty() || animalHealth.trim().isEmpty() || animalAge.trim().isEmpty()))) {
+                EndangeredAnimal endangeredAnimal = new EndangeredAnimal(animalName, animalHealth, animalAge);
+                endangeredAnimal.save();
+                Sighting newSighting = new Sighting(rangerName, location, endangeredAnimal.getId());
+                newSighting.save();
+            } else if (animalType.equals("NonEndangered") && (!(animalName.trim().isEmpty() || animalHealth.trim().isEmpty() || animalAge.trim().isEmpty()))) {
 
+                NonEndangeredAnimal nonEndangeredAnimal = new NonEndangeredAnimal(animalName);
+                nonEndangeredAnimal.save();
+                Sighting newSighting = new Sighting(rangerName, location, nonEndangeredAnimal.getId());
+                newSighting.save();
+            }else if (!(location.trim().isEmpty())) {
+
+                Location newLocation = new Location(location);
+                newLocation.save();
+            }else {
+                System.out.println("Please fill in all the fields");
+
+        }
+            List<Sighting> allSighting = Sighting.all();
+            List<EndangeredAnimal> allEndangeredAnimal= EndangeredAnimal.all();
+            List<NonEndangeredAnimal> allNonEndangeredAnimal= NonEndangeredAnimal.all();
+            List<Location> allLocation= Location.all();
+            List<Ranger> allRanger= Ranger.all();
+            model.put("sighting", allSighting);
+            model.put("endangeredAnimal", allEndangeredAnimal);
+            model.put("nonEndangeredAnimal", allNonEndangeredAnimal);
+            model.put("location", allLocation);
+            model.put("ranger", allRanger);
+            return new ModelAndView(model, "success.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/dashboards", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("sighting", Sighting.all());
+            model.put("endangeredAnimal", EndangeredAnimal.all());
+            model.put("nonEndangeredAnimal", NonEndangeredAnimal.all());
+            model.put("location", Location.all());
+            model.put("ranger", Ranger.all());
+            return new ModelAndView(model, "dashboard.hbs");
+        }, new HandlebarsTemplateEngine());
     }
+
 }
