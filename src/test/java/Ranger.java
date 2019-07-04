e188aab25ce30c19841b1b7f793c60baa7a47564
Annette Odingo <odingoannette@gmail.com>
2019-07-04 06:15:05 +0300

    Add test to confirm Ranger instantiates correctly and test for getters

--- src/test/java/Ranger.java
+++ src/test/java/Ranger.java
@@ -4,5 +4,21 @@ public class Ranger {
     private int badgeNumber;
     private int sightingId;
 
+    public Ranger(String name, int badgeNumber, int sightingId) {
+        this.name = name;
+        this.badgeNumber = badgeNumber;
+        this.sightingId = sightingId;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getBadgeNumber() {
+        return badgeNumber;
+    }
+
+    public int getSightingId() {
+        return sightingId;
+    }
 }
--- src/test/java/RangerTest.java
+++ src/test/java/RangerTest.java
@@ -1,5 +1,33 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RangerTest {
 
+    @Test
+    public void ranger_InstantiatesCorrectly_true() {
+        Ranger newRanger = setupNewRanger();
+        assertEquals(true, newRanger instanceof Ranger);
+    }
+    public Ranger setupNewRanger(){
+        return new Ranger("Apiyo", 2, 1);
+    }
+
+    @Test
+    public void rangerGetNames_Apiyo() {
+        Ranger newRanger = setupNewRanger();
+        assertEquals("Apiyo", newRanger.getName());
+    }
+
+    @Test
+    public void rangerGetBadgeNumber_2() {
+        Ranger newRanger = setupNewRanger();
+        assertEquals(2, newRanger.getBadgeNumber());
+    }
+
+    @Test
+    public void rangerGetSightingId() {
+        Ranger newRanger = setupNewRanger();
+        assertEquals(1, newRanger.getSightingId());
+    }
 }
